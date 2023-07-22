2ae53475df869775ae67d75a12459c00422fa1d0
Mvic-net <mvic.net@gmail.com>
2023-07-22 12:56:08 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,10 @@
-const ulElements = document.querySelectorAll(".item");
-console.log("Number of categories:", ulElements.length);
+// const ulElements = document.querySelectorAll(".item");
+// console.log("Number of categories:", ulElements.length);
 
-ulElements.forEach(function (element, index) {
-  const title = element.querySelector("h2");
-  const liElements = element.querySelectorAll("li");
+// ulElements.forEach(function (element, index) {
+//   const title = element.querySelector("h2");
+//   const liElements = element.querySelectorAll("li");
 
-  console.log(`Category: ${title.textContent}`);
-  console.log(`Elements: ${liElements.length}`);
-});
+//   console.log(`Category: ${title.textContent}`);
+//   console.log(`Elements: ${liElements.length}`);
+// });
